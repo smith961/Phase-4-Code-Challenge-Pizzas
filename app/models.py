ca5857f401eee000661477066eea3325087a85a5
smith <smithbabawande@gmail.com>
2023-11-13 12:41:19 +0100

    we making progress

--- app/models.py
+++ app/models.py
@@ -1,18 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy.orm import validates, relationship, backref
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(reffered_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Pizza (db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
+    serialize_rules = ('-restaurant_pizzas', '-restaurants.pizzas')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -20,14 +16,15 @@ class Pizza (db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # restaurants = assocition_proxy('restaurant_pizzas', 'restaurant')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
     restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
 
 class RestaurantPizza (db.Model, SerializerMixin):
     __tablename__ = 'restaurant_pizzas'
+    serialize_rules = ('-restaurant.pizzas', '-pizza.restaurants', '-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
 
     id = db.Column(db.Integer, primary_key = True)
-    pizza_id = db.Column(db.Integer, db.Foreignkey('pizzas.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
     price = db.Column(db.Float)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
@@ -41,7 +38,8 @@ class RestaurantPizza (db.Model, SerializerMixin):
     
 class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
-    # pizzas = association_proxy('restaurant_pizzas', 'pizza')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+    serialize_rules = ('-restaurant_pizzas', '-pizzas.restaurants')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
